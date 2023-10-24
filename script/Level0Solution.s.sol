bd502a9900addc1a3ff0f4ac057da9f0afba1528
Johnny <learnwithjohnny.official@gmail.com>
2023-10-24 14:14:14 +0300

    Complete level 0

--- script/Level0Solution.s.sol
+++ script/Level0Solution.s.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "../src/Level0.sol";
-import "forge-std/Test.sol";
+import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
-contract Level0Solution is Test {
+contract Level0Solution is Script {
+    
+    Level0 public level0 = Level0(0xD6aed426603094B3772a4a16a7F055A46eD109aE);
 
-    Level0 level0 = Level0(0xD6aed426603094B3772a4a16a7F055A46eD109aE);
-
-    function test() external{
-        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+    function run() external {
         console.log("Password: ", level0.password());
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         level0.authenticate(level0.password()); //call authenticate function with the password
         vm.stopBroadcast();
     }
--- src/Fallout.sol
+++ src/Fallout.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin-06/contracts/math/SafeMath.sol";
 
 contract Fallout {
     using SafeMath for uint256;
